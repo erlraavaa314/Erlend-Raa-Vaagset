9f5bcb3288b2358ce4a7d34872598a245abc1f09
erlraavaa214 <erlend.vagset@uib.no>
2024-06-11 11:05:16 +0200

    Lots of things

--- webpage/resources/scripts/script.js
+++ webpage/resources/scripts/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    
     $('.menu-btn').click(function() {
         $('.menu nav').toggleClass('active');  // Only toggle the visibility of the <nav>
         console.log("Navigation menu toggled");
@@ -14,25 +13,20 @@ $(document).ready(function() {
             console.log("Menu toggled");
         });
     });
-    
-    // Toggle visibility of content under each section when the header is clicked
+    // Toggle visibility of content under each section when the h2 header is clicked
     $("section h2").click(function() {
-        console.log("Content visible");
+        console.log("H2 content visibility toggled");
         $(this).next(".content").slideToggle('slow');
         var isExpanded = $(this).attr("aria-expanded") === "true";
         $(this).attr("aria-expanded", !isExpanded);
     });
 
-    // Hide content when it is clicked, unless the click is on a link
-    $("section .content").click(function(event) {
-        if ($(event.target).is('a')) {
-            // If the click is on a link, do not hide the content
-            return;
-        }
-        console.log("Content hidden");
-        $(this).slideUp('slow');
-        $(this).prev("h2").attr("aria-expanded", "false");
-        event.stopPropagation(); // Prevents the event from bubbling up to the h2 click event
+    // Toggle visibility of content under each section when the h3 header is clicked
+    $("section h3").click(function() {
+        console.log("H3 content visibility toggled");
+        $(this).next(".content-box").slideToggle('slow');
+        var isExpanded = $(this).attr("aria-expanded") === "true";
+        $(this).attr("aria-expanded", !isExpanded);
     });
 
     // Ensure that all menu items are always visible in the mobile menu, removing hover dependency
